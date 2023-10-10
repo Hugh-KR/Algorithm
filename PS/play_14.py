1e060025cb4896044e448b45485499422f52d3ab
Hugh <lee2317884@gmail.com>
2023-10-10 16:07:23 +0900

    feat: 문자열 재정렬

---
+++ PS/play_14.py
@@ -0,0 +1,31 @@
+# s = list(input())
+# s.sort()
+
+# alpha = ''
+# digit = 0
+
+# for val in s:
+#     if val.isalpha():
+#         alpha += val
+#     elif val.isdigit():
+#         digit += int(val)
+
+# print(alpha + str(digit))
+
+
+s = input()
+result = []
+value = 0
+K1KA5CB70
+for x in s:
+    if x.isalpha():
+        result.append(x)
+    elif x.isdigit():
+        value += int(x)
+
+result.sort()
+
+if value != 0:
+    result.append(str(value))
+
+print(''.join(result))
