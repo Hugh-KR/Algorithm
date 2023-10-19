a329aed904a76dedfa812f34b3d77fd043f33903
Hugh <lee2317884@gmail.com>
2023-10-19 10:37:33 +0900

    feat: 파이썬 문법정리

--- PS/play_07.py
+++ PS/play_07.py
@@ -10,7 +10,35 @@
 # data = sys.stdin.readline().rstrip()
 # print(data)
 
-score = 85
-result = "Success" if score >= 80 else "Fail"
+# score = 85
+# result = "Success" if score >= 80 else "Fail"
 
-print(result)
+# print(result)
+
+# from itertools import combinations_with_replacement
+
+# data = ['A', 'B', 'C']
+
+# result = list(combinations_with_replacement(data, 2))
+# print(result)
+
+# from collections import Counter
+
+# counter = Counter(['A', 'B', 'C', 'A', 'A', 'C'])
+
+# print(counter['A'])
+# print(counter['B'])
+# print(counter['C'])
+# print(dict(counter))
+
+# import math
+
+# # 최소 공배수(LCM)를 구하는 함수
+# def lcm(a, b):
+#     return (a * b) // math.gcd(a, b)
+
+# a = 21
+# b = 14
+
+# print(math.gcd(21, 14))
+# print(lcm(21, 14))
