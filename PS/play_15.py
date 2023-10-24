6d9e47e41dbaec3a0f2064666c343208a1e2406b
Hugh <lee2317884@gmail.com>
2023-10-24 21:17:44 +0900

    feat: 파이썬 재귀

--- PS/play_15.py
+++ PS/play_15.py
@@ -11,19 +11,28 @@
 # print(stack[::-1])
 # print(stack)
 
-from collections import deque
+# from collections import deque
 
-queue = deque()
+# queue = deque()
 
-queue.append(5)
-queue.append(2)
-queue.append(3)
-queue.append(7)
-queue.popleft()
-queue.append(1)
-queue.append(4)
-queue.popleft()
+# queue.append(5)
+# queue.append(2)
+# queue.append(3)
+# queue.append(7)
+# queue.popleft()
+# queue.append(1)
+# queue.append(4)
+# queue.popleft()
 
-print(queue)
-queue.reverse()
-print(queue)
+# print(queue)
+# queue.reverse()
+# print(queue)
+
+def recursive_function(cnt):
+    if cnt == 10:
+        return
+    
+    print('재귀 함수 호출')
+    recursive_function(cnt+1)
+
+recursive_function(0)
