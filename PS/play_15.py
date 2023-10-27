4f92d5393e66df277abd0bd96537f4d05d1fc18d
Hugh <lee2317884@gmail.com>
2023-10-28 01:14:35 +0900

    refactor: 파이썬 재귀

--- PS/play_15.py
+++ PS/play_15.py
@@ -28,11 +28,23 @@
 # queue.reverse()
 # print(queue)
 
-def recursive_function(cnt):
-    if cnt == 10:
-        return
-    
-    print('재귀 함수 호출')
-    recursive_function(cnt+1)
+# def recursive_function(cnt):
+#     if cnt == 10:
+#         return
 
-recursive_function(0)
+#     print('재귀 함수 호출')
+#     recursive_function(cnt+1)
+
+# recursive_function(0)
+
+
+def recursive_function(i):
+    # 100번째 호출을 했을 떄 종료되도록 종료 조건 명시
+    if i == 100:
+        return
+    print(i, " 번째 재귀함수에서", i + 1, " 번째 재귀함수를 호출합니다.")
+    recursive_function(i + 1)
+    print(i, " 번째 재귀함수를 종료합니다.")
+
+
+recursive_function(1)
