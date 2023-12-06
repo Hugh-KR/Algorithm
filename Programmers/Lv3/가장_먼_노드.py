86368e438e9f6388075f0b27cd85d29ee53db047
Hugh <lee2317884@gmail.com>
2023-12-06 09:50:25 +0900

    feat: 가장 먼 노드.py

---
+++ Programmers/Lv3/가장_먼_노드.py
@@ -0,0 +1,43 @@
+"""
+1. 최단 거리 구하기 위한 다익스트라 구현
+2. 모든 가중치 1로 가정하고 양방향 그래프 생성
+3. 1번 노드 기준 모든 노드 거리 구하기
+"""
+
+import heapq
+
+def solution(n, edge):
+    answer = 0
+    
+    INF = int(1e9)
+    graph = [[] for i in range(n + 1)]
+    distance = [INF] * (n + 1)
+    
+    # 양방향 그래프 생성
+    for n, e in edge:
+        graph[n].append((e, 1))
+        graph[e].append((n, 1))
+        
+        
+    def dijkstra(start):
+        q = []
+        heapq.heappush(q, (0, start))
+        distance[start] = 0
+        while q: # 큐가 비어있지 않다면
+            # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+            dist, now = heapq.heappop(q)
+            # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+            if distance[now] < dist:
+                continue
+            for i in graph[now]:
+                cost = dist + i[1]
+                # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+                if cost < distance[i[0]]:
+                    distance[i[0]] = cost
+                    heapq.heappush(q, (cost, i[0]))
+                    
+            
+    dijkstra(1)
+    # 올바른 최대값 개수 추출 위한 INF 모두 0으로 변경
+    distance = [0 if x == INF else x for x in distance]
+    return distance.count(max(distance))
