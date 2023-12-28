063f8e57632485881d5cefc8302ae4ac260081b0
Hugh <lee2317884@gmail.com>
2023-12-28 16:38:18 +0900

    refactor: 안녕 step1.py
    * combinations 라이브러리로 변경

--- BOJ/BruteForce/안녕/안녕_step1.py
+++ BOJ/BruteForce/안녕/안녕_step1.py
@@ -23,11 +23,11 @@ L = list(map(int, input().split()))
 # 각각의 사람에게 인사를 할 때, 얻은 기쁨
 J = list(map(int, input().split()))
 
+
 # 사람에 따른 소진 채력, 얻은 기쁨 결합
 people = []
 for i in range(n):
     people.append((L[i], J[i]))
-
 
 # 잃은 채력의 합이 100보다 작을 경우
 if sum(L) < 100:
@@ -41,6 +41,8 @@ else:
     for i in range(n):
         # 중복 없이 i개를 뽑는 경우의 수
         combi = list(combinations(people, i+1))
+        print(combi)
+        
         for com in combi:
             max_health = 100
             happy = 0
@@ -51,6 +53,7 @@ else:
             # 현재 최대 채력이 0보다 크고 얻은 기쁨이 최대 기쁨보다 큰 경우 최대 기쁨 갱신
             if max_health > 0 and happy > max_happy:
                 max_happy = happy
+                
     # 최대 기쁨 출력
     print(max_happy)
 
