e1a9ba8c8f7dd3805243a5b085844bf34e5f4a3b
Hugh <lee2317884@gmail.com>
2024-02-22 10:48:29 +0900

    refactor: 동전 0.py
    - 최적화 진행

--- BOJ/동전_0.py
+++ BOJ/동전_0.py
@@ -4,9 +4,9 @@ coins = [int(input()) for _ in range(n)]
 coins.sort(reverse=True)
 
 count = 0
-for i in coins:
-    if k % i != k:
-        count += k // i
-        k = k % i
-
+for coin in coins:
+    count += k // coin
+    k %= coin
+    print(f'coin: {coin}, K: {k}, count: {count}')
+    
 print(count)
