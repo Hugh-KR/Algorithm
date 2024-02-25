fc87b849d8201fc46c3838419c811c5af75335cf
Hugh <lee2317884@gmail.com>
2024-02-25 15:58:57 +0900

    refactor: 미로 탐색.py

--- BOJ/미로_탐색.py
+++ BOJ/미로_탐색.py
@@ -1,53 +1,44 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
-
-adj = []
-for i in range(N):
-    # sys의 개행 문자 제거
-    adj.append(list(map(int, input().strip())))
-
+# sys의 개행 문자 제거
+adj = [list(map(int, input().strip())) for _ in range(N)]
 check = [[False] * M for _ in range(N)]
-
-
-def isValid(ny, nx):
-    return ny < N and ny >= 0 and nx < M and nx >= 0
-
 
 dy = [-1, 0, 1, 0]
 dx = [0, -1, 0, 1]
 
 
+def isValid(ny, nx):
+    return 0 <= ny < N and 0 <= nx < M 
+
 def bfs(y, x):
     q = deque()
-    q.append((y, x))
+    
+    # 마지막에 현재 거리 정보 추가
+    q.append((y, x, 1))
+    # 방문하지 않았을 경우 방문 처리
+    check[y][x] = True
+    
     while q:
-        now_y, now_x = q.popleft()
-        # 방문한 위치일 경우 넘어가기
-        if check[now_y][now_x]: continue
-        # 방문하지 않았을 경우 방문 처리
-        check[now_y][now_x] = True
+        now_y, now_x, now_d = q.popleft()
+        
+        # 인접 행렬의 끝에 도달했을 경우 위치 정보 반환
+        if now_y == N-1 and now_x == M-1: return now_d
+        # 아직 끝이 아닐 경우 위치 1 증가
+        nd = now_d + 1
         
         for i in range(4):
             ny = now_y + dy[i]
             nx = now_x + dx[i]
 
             # 벗어난 범위일 경우 넘어거기
-            if not isValid(ny, nx): continue
-            # 방문한 위치인지 이중 검증
-            if check[ny][nx]: continue
-            
-            if adj[ny][nx]:
-                # 다음 방문할 위치에 1 추가
-                adj[ny][nx] = adj[now_y][now_x] + 1
-                q.append((ny, nx))
+            # 방문한 위치인지 검증
+            if isValid(ny, nx) and adj[ny][nx] and not check[ny][nx]:
+                check[ny][nx] = True
+                q.append((ny, nx, nd))
 
 
-bfs(0, 0)
-
-# for j in adj:
-#     print(j)
-
-print(adj[N - 1][M - 1])
+print(bfs(0, 0))
